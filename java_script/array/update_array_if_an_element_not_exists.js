c431f91cbed99f676af3161de7797461fbd88dcf
Đông Lào Coder <37970614+nguyenatan@users.noreply.github.com>
2018-05-23 10:44:16 +0700

    Update update_array_if_an_element_not_exists.js

--- java_script/array/update_array_if_an_element_not_exists.js
+++ java_script/array/update_array_if_an_element_not_exists.js
@@ -1,11 +1,8 @@
 var array = ['a', 'b', 'c'];
 
 function updateArray(array, element) {
-	if (!~array.indexOf(element)) {
+	if (!~array.indexOf(element))
 		array.push(element);
-		console.log(array);
-		return;
-	}
 	
 	console.log(array);
 }
