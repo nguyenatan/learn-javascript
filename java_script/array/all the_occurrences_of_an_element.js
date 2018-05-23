7b05f78db240e740ba31093fba5f29daa037a983
Đông Lào Coder <37970614+nguyenatan@users.noreply.github.com>
2018-05-23 11:03:06 +0700

    Create all the_occurrences_of_an_element.js

---
+++ java_script/array/all the_occurrences_of_an_element.js
@@ -0,0 +1,15 @@
+var array = ['a', 'b', 'a', 'c', 'a', 'd'];
+
+function occurrenceIndex(array, element) {
+	var indices = [];
+	
+	var index = array.indexOf(element);
+	while (index != -1) {
+		indices.push(index);
+		index = array.indexOf(element, index+1);
+	}
+	
+	return indices;
+}
+
+console.log(occurrenceIndex(array, 'a').reverse());
