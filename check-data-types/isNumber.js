dc223928123ff012c3f3383245dc2be796709838
nat <37970614+nguyenatan@users.noreply.github.com>
2019-01-17 22:31:45 +0700

    Update isNumber.js

--- check-data-types/isNumber.js
+++ check-data-types/isNumber.js
@@ -1,5 +1,4 @@
-// From typeof more things than just an ordinary number will return "number" like NaN and Infinity.
-// To know if a value really is a number the function isFinite is also required.
+// Return if a value is really a number
 function isNumber(value) {
   return typeof value === 'number' && isFinite(value);
 }
