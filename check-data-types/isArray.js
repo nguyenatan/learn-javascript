543e6c405280dc0b0c25f80a270dfcf661d5464f
nat <37970614+nguyenatan@users.noreply.github.com>
2019-01-17 22:34:22 +0700

    Create isArray.js

---
+++ check-data-types/isArray.js
@@ -0,0 +1,7 @@
+// Return if a value is really an array
+function isArray(value) {
+  return value && typeof value === 'object' && value.constructor === Array;
+}
+
+// ES5 actually has a method for this (ie9+)
+Array.isArray(value);
