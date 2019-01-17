3679a660ab9def142ce7f0281911a2fec1984276
nat <37970614+nguyenatan@users.noreply.github.com>
2019-01-17 21:57:45 +0700

    Create isString.js

---
+++ check-data-types/isString.js
@@ -0,0 +1,6 @@
+// A string is always a string so this one is easy.
+// Except if called with new (new String) typeof will instead return "object".
+// So to also include those strings instanceof can be used.
+function isString(value) {
+  return typeof value === "string" || value instanceof String
+}
