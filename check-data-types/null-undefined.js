b37281eab9597fd654d8b34e5e1536cf06dc5f52
nat <37970614+nguyenatan@users.noreply.github.com>
2019-01-18 17:21:52 +0700

    Create null-undefined.js

---
+++ check-data-types/null-undefined.js
@@ -0,0 +1,9 @@
+// Return if a value is null
+function isNull(value) {
+  return value === null;
+}
+
+// Return if a value is undefined
+function isUndefined(value) {
+  return typeof value === 'undefined';
+}
